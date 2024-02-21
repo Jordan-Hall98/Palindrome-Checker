09545cfb773dcd823215f3d7ce634a88add19c70
Jordan <60506289+Jordan-Hall98@users.noreply.github.com>
2024-02-21 17:59:11 +0000

    Add files via upload

---
+++ Palidrome Test solution 2.py
@@ -0,0 +1,12 @@
+def palindrome_checker(inputString):
+  if inputString == inputString[::-1]:
+    return True
+  else:
+    return False
+
+word = input("What word would you like to check to see if it is a palidrome? ")
+
+if palindrome_checker(word):
+  print ("This is a palindrome")
+else: 
+  print ("This is not a palindrome")
